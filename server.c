98046463bda6ab841530769f505add2f0b57274d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-12-31 14:51:01 +0100

    Fix json format

--- server.c
+++ server.c
@@ -113,7 +113,7 @@ void http_api(struct evhttp_request *req, void *data) {
     }
 
     evbuffer_add_printf(res, "{\n");
-    evbuffer_add_printf(res, "  moves: [\n");
+    evbuffer_add_printf(res, "  \"moves\": [\n");
     for (size_t i = 0; i < num_children; i++) {
         char uci[MAX_UCI], san[MAX_SAN];
         move_uci(results[i].move, uci);
