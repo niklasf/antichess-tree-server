0c71978fdc24c02668d9299b58abac7fa4d89eac
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-01-02 12:57:07 +0100

    Add failing test for Nxb2 SAN

--- test.c
+++ test.c
@@ -1,9 +1,11 @@
 #include <assert.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "san.h"
 
 void test_san() {
+    char san[MAX_SAN];
     board_t board;
     board_reset(&board);
 
@@ -12,10 +14,20 @@ void test_san() {
     board_move(&board, move_parse("a2a4"));
     board_move(&board, move_parse("c8a6"));
 
-    char san[MAX_SAN];
-    move_t move = move_parse("f1a6");
-    board_san(&board, move, san);
+    move_t Bxa6 = move_parse("f1a6");
+    board_san(&board, Bxa6, san);
     assert(strcmp(san, "Bxa6") == 0);
+
+    board_move(&board, Bxa6);
+    board_move(&board, move_parse("b8a6"));
+    board_move(&board, move_parse("b2b4"));
+    board_move(&board, move_parse("a6b4"));
+    board_move(&board, move_parse("d1h5"));
+
+    move_t Nxc2 = move_parse("b4c2");
+    board_san(&board, Nxc2, san);
+    puts(san);
+    assert(strcmp(san, "Nxc2") == 0);
 }
 
 int main() {
